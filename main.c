68549aeeca8e0c8e4f2ca953dc097e70c46ba789
viniciius14 <rodri.f.ribeiro@gmail.com>
2023-01-12 20:24:53 +0000

    commit

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 typedef struct{
 	struct TreeNode *left; // 0
 	struct TreeNode *right; // 1
 	char* letters;
 }TreeNode;
+
 // struct size is 12 if on 32 bit machine
 //struct size is 20 8 per pointer + 1 (4) for the char
 // now its 24?
@@ -37,40 +39,44 @@ char *huffman_encode(const char *to_encode, TreeNode *root){
 */
 
 
-char *huffman_encode(const char* to_encode, TreeNode *root){
-	// count letters
-	/*
-	make an array of size to_encode
-	*/
-	size_t string_size = sizeof(to_encode)/sizeof(to_encode[0]);
-	// make a 2D array where the first row is letters in int and second one is the number of ocurrences
-	
-	int letter_counter[string_size][1]; // max size should be 94 because thats the max number of elems in ascii table that we can receive though text
-	memset(letter_counter, 0, sizeof(letter_counter));
-	
-	int added_elems = 0;		
-	
+int huffman_encode(const char* to_encode, TreeNode *root){
+  // make a 2D array where the first row is letters in __uint8_t and second one is the number of ocurrences
   /* Example: 
 	________________
 	|a|b|e|f|...| | |
     |3|4|2|1|...| | |
 	‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
   */
-  
+	size_t string_size = strlen(to_encode);	
+	// max size should be 94 because thats the max number of elems in ascii table that we can receive though text
+    uint8_t letter_counter[2][string_size - 1]; 
+	memset  (letter_counter, 0, sizeof(letter_counter));
+	
+
+    // add elements to 2D array
+	int added_elems = 0;		
+	  
 	for(int i = 0 ; i != string_size ; i++){
 		for(int j = 0 ; j !=  string_size; j++){
-			if( letter_counter[j][1] == 0) { // means we tranversed full array and didnt find the letter 
-				letter_counter[j][0] = (int)to_encode[i];
-				letter_counter[j][1] = 1;
+			if(letter_counter[1][j] == 0) { // means we tranversed full array and didnt find the letter 
+				letter_counter[0][j] = (uint8_t)to_encode[i];
+				letter_counter[1][j] = 1;
 				added_elems++;
 				break;
 			}
-			if(letter_counter[j][1] != 0){ // we found the element
-				letter_counter[j][1]++;
+			if(letter_counter[0][j] == (uint8_t)to_encode[i] && letter_counter[1][j] != 0){ // we found the element
+				letter_counter[1][j]++;
 				break;
 			}
 		}
 	}
+
+
+    for(int i = 0 ; i != added_elems ; i++){
+        printf("%c\t%d\n",(char)letter_counter[0][i], letter_counter[1][i]);
+    }
+
+
 	
 	// need to sort array to get this
 	/* Example: 
@@ -80,20 +86,20 @@ char *huffman_encode(const char* to_encode, TreeNode *root){
 	‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 	*/
 	// lazy and crappy bubble sort
-	for(int j = 1 ; j != added_elems ; j++){
-		for(int i = 1 ; i!= added_elems ; i++){
-			if(letter_counter[i][1] > letter_counter[i - 1][1]){
-				int temp_letter = letter_counter[i][0];
-				int temp_value = letter_counter[i][1];
+// 	for(int j = 1 ; j != added_elems ; j++){
+// 		for(int i = 1 ; i!= added_elems ; i++){
+// 			if(letter_counter[i][1] > letter_counter[i - 1][1]){
+// 				int temp_letter = letter_counter[i][0];
+// 				int temp_value = letter_counter[i][1];
 				
-				letter_counter[i][0] = letter_counter[i - 1][0];
-				letter_counter[i][1] = letter_counter[i - 1][1];
+// 				letter_counter[i][0] = letter_counter[i - 1][0];
+// 				letter_counter[i][1] = letter_counter[i - 1][1];
 				
-				letter_counter[i - 1][0] = temp_letter;
-				letter_counter[i - 1][1] = temp_value;
-			}
-		}
-	}
+// 				letter_counter[i - 1][0] = temp_letter;
+// 				letter_counter[i - 1][1] = temp_value;
+// 			}
+// 		}
+// 	}
 
 	
 	
@@ -108,14 +114,27 @@ char *huffman_encode(const char* to_encode, TreeNode *root){
 
 
 int main(void){
+
+
+    TreeNode *root;
+    /*
+    const char *encoded = huffman_encode(const char*, TreeNode *root);
+
+    print_tree(TreeNode *root);
+
+    const char *original = huffman_decode(const char* encoded, TreeNode *root); 
+    */
 	
-	printf("sizeof struct TreeNode = %zu",sizeof(TreeNode));
+	printf("\nsizeof struct TreeNode = %zu\n",sizeof(TreeNode));
+	huffman_encode("acbc", root);
+    // printf("return from to encode = %d", );
+
+
 	
+	// TreeNode *root;
+	// memset(root, 1, sizeof(TreeNode));
 	
-	TreeNode *root;
-	memset(root, 1, sizeof(TreeNode));
-	
-	char *encoded = huffman_encode("string_teste", root);
+	// char *encoded = huffman_encode("string_teste", root);
 	
 	return 0;
 
